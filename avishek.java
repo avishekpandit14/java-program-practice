2f07bbd916ac215d32f834ae262e5912b479f5e5
Avishek Kumar Pandit <avishekpandit51@gmail.com>
2025-08-05 20:06:57 +0530

    Update avishek.java

---
+++ avishek.java
@@ -0,0 +1,72 @@
+
+import java.util.Scanner;
+
+
+
+public class condition{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+
+        // area of rectangle
+        // int length=sc.nextInt();
+        // int breath=sc.nextInt();
+        // int area=length*breath;
+        // System.out.println("Area="+area);
+
+        //sum of a,b and subtract by c
+        // int a=sc.nextInt();
+        // int b=sc.nextInt();
+        // int c=sc.nextInt();
+        // int answer=a+b-c;
+        // System.out.println("Answer"+answer);
+
+        //area of circle
+
+        // int radius=sc.nextInt();
+        // float area=3.14f+(radius*radius);
+        // System.out.println("Area"+area);
+
+        // int p=sc.nextInt();
+        // int r=sc.nextInt();
+        // int t=sc.nextInt();
+        // int si=(p*r*t)/100;
+        // System.out.println("Simple interest"+si);
+
+
+        //check the word is vowel or not 
+
+        // char ch = 'e';
+        // if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+        //     System.out.println("Vowel");
+        // else
+        //     System.out.println("Consonant");
+
+
+        //Check if a number is positive, negative, or zero 
+
+      //   int a=sc.nextInt();
+      //   if(a>0){
+      //     System.out.println("Positive");
+      //   }
+      //   else if(a<0){
+      //     System.out.println("Negative");
+      //   }
+
+      // else{
+      //   System.out.println("zero");
+      // }
+
+
+      
+
+    }
+
+
+    
+
+
+
+    }
+    
+
+
